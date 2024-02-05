aed270f845694d7151e14cc8b62a7cf75b18c4bc
JwanMardini <jwan.99.mardini@gmail.com>
2024-02-05 18:52:36 +0100

    Added media queries

--- index.js
+++
@@ -1,9 +0,0 @@
-// navbar configuration
-const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
-const navbarLinks = document.getElementsByClassName('links');
-toggleButton.addEventListener('click', () => {
-    for (let i = 0; i < navbarLinks.length; i++) {
-        navbarLinks[i].classList.toggle('active');
-    }
-});
-
